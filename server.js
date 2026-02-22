7ae6e8f5dc7a5c6e225a0620d0d9efe4ebc2d830
keileadsall-ai <keileadsall@gmail.com>
2026-02-22 10:31:29 -0500

    server.js
    Added CORS middleware to allow requests from any origin.

--- server.js
+++ server.js
@@ -3,7 +3,11 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    next();
+});
+
 app.use(express.static(path.join(__dirname, 'public')));
 app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'public', 'index.html')));
 app.listen(PORT, () => console.log(`The Vault is Open on port ${PORT}`));
-
