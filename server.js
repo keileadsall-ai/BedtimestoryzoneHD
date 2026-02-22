575ea91f970ecf30f2c8eeafda369a2479c2bdb0
keileadsall-ai <keileadsall@gmail.com>
2026-02-22 13:51:29 -0500

    server.js

--- server.js
+++ server.js
@@ -4,9 +4,6 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'public', 'index.html')));
 
 app.listen(PORT, () => console.log(`The Ritual has begun on port ${PORT}`));
